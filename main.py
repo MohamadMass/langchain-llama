c408c20e01b7d2b09a5af6e33f9b37af5d85be1d
MohamadMassalha <mohamad.massalha.93@gmail.com>
2024-01-09 23:53:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 import time
 
 prompt_template = """Use the following pieces of context to answer the users question.
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
+If you don't know the answer, use your knowldge to come up with answer that is brief and straight to the point.
 ALWAYS return a "SOURCES" part in your answer.
 The "SOURCES" part should be a reference to the source of the document from which you got your answer.
 The example of your response should be:
